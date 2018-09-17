5dce2146131076cf8bd160f9587fd9070635b39f
slim <smedini@gmail.com>
2018-09-17 13:05:55 +0200

    player

--- src/play.js
+++ src/play.js
@@ -1,12 +1,12 @@
-import Vue from 'vue';
-import Play from './vue/Play';
-import {navTo, play} from "./js/playerControl";
+import Vue from 'vue'
+import Play from './vue/Play'
+import {navTo, play} from "./js/playerControl"
 
-const endEvent = new Event('end',{bubbles:true});
+const endEvent = new Event('end', {bubbles: true})
 
 //pour usage dans une appli vue
-export const nav = navTo;
-export const start = play;
+export const nav = navTo
+export const start = play
 
 //usage dans une page html avec balise script
 export const init = (el, film, ready) => {
@@ -14,28 +14,24 @@ export const init = (el, film, ready) => {
         el,
         components: {Play},
         data: function () {
-            return {film: null, dom: null, endcb:null};
+            return {film: null, dom: null, endcb: null}
         },
-        template: '<Play :film="film" ref="qzszsx"/>',
+        template: '<Play :film="film" ref="film"/>',
         mounted: function () {
-            this.dom = this.$refs.qzszsx.el;
-            this.film = film;
+            this.dom = this.$refs.film.el
+            this.film = film
             ready({
                 start: () => play(this.film),
                 nav: i => navTo(this.film, i),
                 end: endcb => this.endcb = endcb
-            });
+            })
         },
         watch: {
             'film.f.player.playing': function (v) {
                 if (!v) {
-                    this.endcb();
+                    this.endcb()
                 }
             }
         }
-    });
-};
-
-
-//utiliser cela pour les applis vue:
-//https://unpkg.com/package@version/file
+    })
+}
